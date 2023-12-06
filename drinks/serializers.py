728b86ed6e738aef5c96e1ebff037a9ddf469829
Guylherme Franklin <guylhermefranklin95@gmail.com>
2023-12-06 13:02:01 -0300

    first commit

---
+++ drinks/serializers.py
@@ -0,0 +1,8 @@
+from .models import Drink
+from rest_framework import serializers
+
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id', 'name', 'description']
